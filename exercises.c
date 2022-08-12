89a3b0117fa3afb8b783cb4d02db2e4ba00025e0
Replit user <>
2022-08-12 21:48:47 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -78,10 +78,10 @@ typedef struct {
 } Vector;
 
 Vector* crearVector(int n) {
-  Vector vector;
+  Vector* vector;
   vector.capacidad = n;
   vector.datos = (int*)calloc(vector.capacidad,sizeof(int));
-  return &vector;
+  return vector;
 }
 
 /*
