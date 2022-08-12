edadfd1f1dfadfd6dbe06fa44fe1a6b9cdb2ecff
Replit user <>
2022-08-12 21:30:23 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -79,12 +79,8 @@ typedef struct {
 
 Vector * crearVector(int n) {
   Vector* v;
-  int i;
   v->capacidad = n;
-  v->datos = (int*) malloc (sizeof(int)*n);
-  for (i=0;i<v->capacidad;i++){
-    v->datos[i]=0;
-  }
+  v->datos = (int*) calloc (n,sizeof(int));
   return v;
 }
 
@@ -94,7 +90,7 @@ Programe la función void asignarValor(Vector * v, int i, int valor),
 la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) {
-  
+  v->datos[i]=valor;
 }
 
 /*
